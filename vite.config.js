ffad41d3019ba3d434b5c79a9ea63586de1dc3c8
LesterPowertrac <oliveriojohnlester.powertrac@gmail.com>
2024-08-24 14:55:22 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend/monitoring",
+  base: "/frontend/",
   plugins: [react()],
   server: {
     port: 3001, // Set the port to 3001
